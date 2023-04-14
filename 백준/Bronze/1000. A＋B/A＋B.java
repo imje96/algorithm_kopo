34d699b503ba304785adc9dd29351bb4592645af
imje96 <127702320+imje96@users.noreply.github.com>
2023-04-15 08:46:42 +0900

    [Bronze V] Title: A+B, Time: 216 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		int A, B;
+		A = sc.nextInt();
+		B = sc.nextInt();
+		if(A<10 && B<10){ 
+		System.out.println(A + B);
+	    }
+    }      
+}
+ 
